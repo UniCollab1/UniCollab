266b36516f7868d1cb89f376ac738f3fddb68264
Spidy <rajkalathiya69@gmail.com>
2020-12-26 12:27:01 +0530

    Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.light(),
       initialRoute: '/',
       routes: {
         '/': (context) => Login(),
