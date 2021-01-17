f992f1ee10346931e320d4abc0d49b851bb151e1
shrey <shreybhadiyadara33@yahoo.com>
2021-01-17 11:14:06 +0530

    Added Dialog for newly Registered user. Some refinement in Drawer.

--- lib/assignments.dart
+++ lib/assignments.dart
@@ -4,7 +4,9 @@ class Assignment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Assignments'),
+      child: Center(
+        child: Text('Assignments'),
+      ),
     );
   }
 }
--- lib/recents.dart
+++ lib/recents.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
-class Recent extends StatelessWidget {
+class Recent extends StatefulWidget {
+  @override
+  _RecentState createState() => _RecentState();
+}
+
+class _RecentState extends State<Recent> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Recents'),
+      child: Center(
+        child: Text('Recent'),
+      ),
+    );
+  }
+}
+
+class RecentFloat extends StatefulWidget {
+  @override
+  _RecentFloatState createState() => _RecentFloatState();
+}
+
+class _RecentFloatState extends State<RecentFloat> {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return SimpleDialog(
+              children: [
+                ListTile(
+                  title: Text('Join Class'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text('Create Class'),
+                  onTap: () {},
+                ),
+              ],
+            );
+          },
+        );
+      },
+      child: Icon(Icons.add),
     );
   }
 }
