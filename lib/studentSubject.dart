8aa26c2d3043d7503b696eb8b53c07fba85f73a0
shrey <shreybhadiyadara33@yahoo.com>
2021-01-28 14:43:56 +0530

    updated student and teacher pages with StreamBuilder.

--- lib/studentSubject.dart
+++ lib/studentSubject.dart
@@ -59,56 +59,114 @@ class _GetClassState extends State<GetClass> {
     return lol;
   }
 
+  Future<dynamic> getNotice() async {
+    var lol = await fireStore
+        .collection('classes')
+        .doc(widget.code)
+        .collection('notice')
+        .get();
+    return lol.docs;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: StreamBuilder<QuerySnapshot>(
-        stream: getData().snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(
-                semanticsLabel: 'loading your classes',
-              ),
-              // ignore: missing_return
-            );
-          } else {
-            print(snapshot.data);
-            return SizedBox(
-              height: 100.0,
-              child: ListView(
-                children: snapshot.data.docs.map((DocumentSnapshot document) {
-                  return Container(
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                StudentMaterialPage(
-                                    document.data(), widget.code),
-                          ),
-                        );
-                      },
-                      child: Card(
-                        clipBehavior: Clip.antiAlias,
-                        child: Column(
-                          children: [
-                            ListTile(
-                              leading: Icon(Icons.description),
-                              title:
-                                  Text('Material: ' + document.data()["title"]),
+      child: Column(
+        children: [
+          StreamBuilder<QuerySnapshot>(
+            stream: getData().snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    semanticsLabel: 'loading your classes',
+                  ),
+                  // ignore: missing_return
+                );
+              } else {
+                print(snapshot.data);
+                return SizedBox(
+                  height: 100.0,
+                  child: ListView(
+                    children:
+                        snapshot.data.docs.map((DocumentSnapshot document) {
+                      return Container(
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    StudentMaterialPage(
+                                        document.data(), widget.code),
+                              ),
+                            );
+                          },
+                          child: Card(
+                            clipBehavior: Clip.antiAlias,
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  leading: Icon(Icons.description),
+                                  title: Text(
+                                      'Material: ' + document.data()["title"]),
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                );
+              }
+            },
+          ),
+          FutureBuilder(
+            future: getNotice(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    semanticsLabel: 'loading your notice',
+                  ),
+                  // ignore: missing_return
+                );
+              } else {
+                return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  StudentMaterialPage(
+                                      snapshot.data[index], widget.code),
+                            ),
+                          );
+                        },
+                        child: Card(
+                          clipBehavior: Clip.antiAlias,
+                          child: ListTile(
+                            leading: Icon(Icons.description),
+                            title: Text(
+                              'Notice: ' + snapshot.data[index].get("title"),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                }).toList(),
-              ),
-            );
-          }
-        },
+                    );
+                  },
+                );
+              }
+            },
+          ),
+        ],
       ),
     );
   }
