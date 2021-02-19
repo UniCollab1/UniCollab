e2a5d7d4c5431e64a86c0de29c2c27b29bf939a5
Spidy-crypto <rajkalathiya699@gmail.com>
2021-02-20 01:36:04 +0530

    Validation for grades

--- lib/app/home/CreateDialog.dart
+++ lib/app/home/CreateDialog.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -80,7 +81,7 @@ class _CreateDialogState extends State<CreateDialog> {
                     filled: true,
                     labelText: 'Class title',
                     errorText:
-                        titleValidation ? 'Title can not be empty' : null,
+                    titleValidation ? 'Title can not be empty' : null,
                   ),
                   textCapitalization: TextCapitalization.words,
                   onChanged: (value) {
@@ -96,7 +97,7 @@ class _CreateDialogState extends State<CreateDialog> {
                     filled: true,
                     labelText: 'Subject',
                     errorText:
-                        subjectValidation ? 'Subject can not be empty' : null,
+                    subjectValidation ? 'Subject can not be empty' : null,
                   ),
                   textCapitalization: TextCapitalization.words,
                   onChanged: (value) {
